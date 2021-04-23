92f8be1a13a2d3b25e04310396f4eb35b750d480
Rami <ramiisinportland@gmail.com>
2021-04-23 16:41:48 -0700

    final commit for the day

--- src/services/currency-exchange.js
+++ src/services/currency-exchange.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {  
   static getCurrency(currencyFrom, currencyTo, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/d047175b1816fac751087e8a/latest/USD${currencyFrom}&to=${currencyTo}&amount=${amount}`, { mode: 'cors' })
+    return fetch(`https://v6.exchangerate-api.com/v6/d047175b1816fac751087e8a/latest/USD`)
       .then(function(response) {// Error handling block
         if (!response.ok) {
           throw Error(response.statusText);
