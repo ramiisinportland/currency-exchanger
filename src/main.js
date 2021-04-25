5a7897142ddc177b938b452054f1bdefe8cf7d9e
Rami <ramiisinportland@gmail.com>
2021-04-25 13:14:05 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,29 +2,57 @@ import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import './css/styles.css'
-import CurrencyService from './services/currencyService';
+import CurrencyService from './currencyService.js';
 
-function getElements(response) {
-  if (response.meta.code === 200) {
-    $("#show-result").text(response)
+function emiratiDirham(response, dollarAmount) {
+  if (response) {
+    let exchangeRate = response.conversion_rates.AED
+    let exchange = (dollarAmount * exchangeRate).toFixed(2);
+    $("#showRate").text(`${exchange} Emirati Dirham`);
   } else {
-    $('.showErrors').text(`There was an error: ${response.status}`);
-    }
+    $('#showError').text(`Something Went Wrong: ${response.message}`);
+  }
+}
+
+function azerbaijanManats(response, dollarAmount) {
+  if (response) {
+    let exchangeRate = response.conversion_rates.AZN
+    let exchange = (dollarAmount * exchangeRate).toFixed(2);
+    $("#showRate").text(`${exchange} Azerbaijan Manats`);
+  } else {
+    $('#showError').text(`Something Went Wrong: ${response.message}`);
+  }
+}
+
+function euro(response, dollarAmount) {
+  if (response) {
+    let exchangeRate = response.conversion_rates.EUR
+    let exchange = (dollarAmount * exchangeRate).toFixed(2);
+    $("#showRate").text(`${exchange} EUR`);
+  } else {
+    $('#showError').text(`Something Went Wrong: ${response.message}`);
+  }
 }
 
 
-$(document).ready(function() { //1
-  $("#currencySelector").submit(function(event) {
-    event.preventDefault();
-    console.log("clicked")
-    const currencyOne = $("#currencyOne option:selected").val(); 
-    console.log(currencyOne)
-    const currencyTwo = $("#currencyTwo option:selected").val();
-    console.log(currencyTwo)
-    const conversionAmount = parseInt($("#amountToConvert").val());
-    CurrencyService.getCurrency(currencyOne, currencyTwo, conversionAmount) 
-    .then(function(response) { 
-      getElements(response); 
-    });
-  });
-});
+function britishPound(response, dollarAmount) {
+  if (response) {
+    let exchangeRate = response.conversion_rates.GBP
+    let exchange = (dollarAmount * exchangeRate).toFixed(2);
+    $("#showRate").text(`${exchange} British pound`);
+  } else {
+    $('#showError').text(`There was an error: ${response.message}`);
+  }
+}
+
+function mexicanPeso(response, dollarAmount) {
+  if (response) {
+    let exchangeRate = response.conversion_rates.MXN
+    let exchange = (dollarAmount * exchangeRate).toFixed(2);
+    $("#showRate").text(`${exchange} Mexican Peso`);
+  } else {
+    $('#showError').text(`Something Went Wrong: ${response.message}`);
+  }
+}
+
+
